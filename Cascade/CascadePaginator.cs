1efe4c57fb67e15f9b0d0700acbdcff2f9bb57fc
Gary McGhee <gary@huey.co>
2022-09-06 11:39:38 +0800

    CascadePaginator.cs now has HighestPage, LastPageLoaded and Loading properties

--- Cascade/CascadePaginator.cs
+++ Cascade/CascadePaginator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,9 @@ namespace Cascade {
 		public object Criteria { get; }
 		public string CollectionPrefix { get; }
 		public int PerPage { get; }
+		public int HighestPage { get; protected set; } = -1;
+		public bool LastPageLoaded { get; protected set; }
+		public bool Loading { get; protected set; }
 
 		public CascadePaginator(CascadeDataLayer cascade, object criteria, string collectionPrefix, int perPage) {
 			Cascade = cascade;
@@ -21,13 +25,25 @@ namespace Cascade {
 			return CollectionPrefix+"__"+page.ToString("D3");
 		}
 
-		//async 
-		public Task<IEnumerable<Model>> Query(int page, int? freshnessSeconds = null) {
-			var criteriaWithPagination = AddPaginationToCriteria(Criteria,page);
-			queriedPages.Add(page);
-			return Cascade.Query<Model>(collectionName(page), criteriaWithPagination, freshnessSeconds);
+		public async Task<IEnumerable<Model>> Query(int page, int? freshnessSeconds = null) {
+			if (Loading)
+				throw new ConstraintException("CascadePaginator Query cannot be re-entered when it has not completed");
+			try {
+				Loading = true;
+				var criteriaWithPagination = AddPaginationToCriteria(Criteria,page);
+				queriedPages.Add(page);
+				var results = await Cascade.Query<Model>(collectionName(page), criteriaWithPagination, freshnessSeconds);
+				if (!LastPageLoaded && page > HighestPage) {
+					HighestPage = page;
+					LastPageLoaded = results.Count() < PerPage;
+				}
+				return results;
+			}
+			finally {
+				Loading = false;
+			}
 		}
-
+		
 		protected abstract object AddPaginationToCriteria(object criteria, int page);
 
 		public async Task Clear() {
@@ -35,12 +51,14 @@ namespace Cascade {
 				var key = collectionName(page);
 				await Cascade.ClearCollection(key);
 			}
+			HighestPage = -1;
+			LastPageLoaded = false;
 		}
 
 		public async Task Refresh(int freshnessSeconds = 0) {
-			foreach (var page in queriedPages) {
-				await Query(page, freshnessSeconds);
-			}
+			// foreach (var page in queriedPages) {
+			// 	await Query(page, freshnessSeconds);
+			// }
 		}
 	}
 }
