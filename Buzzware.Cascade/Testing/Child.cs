20e390349c4d8e856121d2a27afd9a1852e82a61
Gary McGhee <gary@huey.co>
2024-08-09 17:13:53 +0800

    fixed properties

--- Buzzware.Cascade/Testing/Child.cs
+++ Buzzware.Cascade/Testing/Child.cs
@@ -1,22 +1,49 @@
-﻿// ReSharper disable ParameterHidesMember
-
-
-
-
-namespace Buzzware.Cascade.Testing {
+﻿namespace Buzzware.Cascade.Testing {
 	public class Child : SuperModel {
-		
+   
 		[Cascade.CascadeId]
-		public string id { get; set; }
-		public int? parentId { get; set; }
-		
+		public string id {
+			get => GetProperty(ref _id);
+			set => SetProperty(ref _id, value);
+		}
+		private string _id;
+
+		public int? parentId {
+			get => GetProperty(ref _parentId);
+			set => SetProperty(ref _parentId, value);
+		}
+		private int? _parentId;
+   
 		[Cascade.BelongsTo(idProperty: "parentId")]
-		public Parent? Parent { get; set; }
-		
-		public double? weight { get; set; }
-		public double? power { get; set; }
-		public int age { get; set; }
-		public long updatedAtMs { get; set; }
+		public Parent? Parent {
+			get => GetProperty(ref _parent);
+			set => SetProperty(ref _parent, value);
+		}
+		private Parent? _parent;
+   
+		public double? weight {
+			get => GetProperty(ref _weight);
+			set => SetProperty(ref _weight, value);
+		}
+		private double? _weight;
+
+		public double? power {
+			get => GetProperty(ref _power);
+			set => SetProperty(ref _power, value);
+		}
+		private double? _power;
+
+		public int age {
+			get => GetProperty(ref _age);
+			set => SetProperty(ref _age, value);
+		}
+		private int _age;
+
+		public long updatedAtMs {
+			get => GetProperty(ref _updatedAtMs);
+			set => SetProperty(ref _updatedAtMs, value);
+		}
+		private long _updatedAtMs;
 
 		public Child() {
 		}
--- Buzzware.Cascade/Testing/Parent.cs
+++ Buzzware.Cascade/Testing/Parent.cs
@@ -1,21 +1,42 @@
 ﻿using System.Collections.Generic;
-
 namespace Buzzware.Cascade.Testing {
 	public class Parent : SuperModel {
-		
+        
 		[Cascade.CascadeId]
-		public long id { get; set; }
-		
+		public long id {
+			get => GetProperty(ref _id);
+			set => SetProperty(ref _id, value);
+		}
+		private long _id;
+        
 		[Cascade.HasMany(foreignIdProperty: "parentId")]
-		public IEnumerable<Child>? Children { get; set; }
-		
-		public string? colour { get; set; }
-		public string? Size { get; set; }
-		public long updatedAtMs { get; set; }
+		public IEnumerable<Child>? Children {
+			get => GetProperty(ref _children);
+			set => SetProperty(ref _children, value);
+		}
+		private IEnumerable<Child>? _children;
+        
+		public string? colour {
+			get => GetProperty(ref _colour);
+			set => SetProperty(ref _colour, value);
+		}
+		private string? _colour;
+
+		public string? Size {
+			get => GetProperty(ref _size);
+			set => SetProperty(ref _size, value);
+		}
+		private string? _size;
+
+		public long updatedAtMs {
+			get => GetProperty(ref _updatedAtMs);
+			set => SetProperty(ref _updatedAtMs, value);
+		}
+		private long _updatedAtMs;
 
 		public Parent() {
 		}
-		
+        
 		public Parent withChanges(
 			string? colour = null,
 			string? size = null,
